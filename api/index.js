3f4263a574c2ccb5c0f96f7f96d6bdcffa5dfe6f
EduRossB <edu_barba@outlook.com>
2023-09-09 11:35:58 +0200

    vercel config 8

--- api/index.js
+++ api/index.js
@@ -9,6 +9,24 @@ app.use(express.json())
 
 const port = process.env.PORT || 3001
 
+app.get('/', (req, res) => {
+    // Genera la respuesta HTML directamente en esta función
+    const htmlResponse = `
+      <html>
+        <head>
+          <title>Mi Página Principal</title>
+        </head>
+        <body>
+          <h1>Bienvenido a mi página principal</h1>
+          <p>Este es un ejemplo de respuesta HTML generada dinámicamente.</p>
+        </body>
+      </html>
+    `;
+  
+    // Envía la respuesta HTML al cliente
+    res.send(htmlResponse);
+  });
+
 app.use('/api',require('../routes/coments'))
 app.listen(port)
 
